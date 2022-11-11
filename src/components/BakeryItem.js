08e682f11d1c85683c772fad24bae273757f31e5
Claire Oberg <claire_oberg@brown.edu>
2022-11-10 21:02:25 -0500

    finished

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,16 @@
 // TODO: create a component that displays a single bakery item
+
+function BakeryItem(props) {
+    return (
+    <div className="BakeryItem">
+        <h3>{props.item.name}</h3>
+        <p>{props.item.description}</p>
+        <p>Price: {props.item.price}</p>
+        <img src={props.item.image}/>
+        <button onClick={() => {props.addToCart(props.item)}}>Add to cart</button>
+        
+    </div>)
+    
+}
+
+export default BakeryItem;
